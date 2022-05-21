30d1ec8d5643e9945093108feb47b3d91d7114d8
Aleksandr Klevko <troggor7@gmail.com>
2022-05-21 18:25:37 +0300

    test BE requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public onGetEntries() {
+    // TODO this is just an example, remove/rewrite when actual code design is ready
+    this.http.get('http://localhost:3000/entries', { observe: 'response' }).subscribe((resp) => {
+      console.log(resp);
+    });
+  }
+
+}
